cc60f3d1819c6afa2aa75364db421444d2a1e1c0
mikhita <mikheilispiriani@yahoo.com>
2023-04-09 18:53:57 +0400

    I  defined PropTypes for one of the components of my application, and added ESlint to the project. I defined the configuration according to my liking. I  fixed all of the linter errors.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-import NotificationRed from "./NotificationRed"
+import NotificationRed from './NotificationRed'
 import loginService from '../services/login'
 import blogService from '../services/blogs'
+import PropTypes from 'prop-types'
 
 
 const LoginForm = ({
   errorMessageRed, setErrorMessageRed, setUser
- }) => {
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleLogin = async (event) => {
     event.preventDefault()
-    
     try {
       const user = await loginService.login({
         username, password,
@@ -28,37 +28,43 @@ const LoginForm = ({
       if (error.response.status === 500) {
         console.log(error.response.status)
         setErrorMessageRed('wrong username or password')
-      } 
+      }
       setTimeout(() => {
         setErrorMessageRed(null)
       }, 5000)
     }
   }
- return (
-   <div>
-    <NotificationRed message={errorMessageRed} />
-     <h2>Login</h2>
 
-     <form onSubmit={handleLogin}>
-       <div>
-         username
-         <input
-           value={username}
-           onChange={({ target }) => setUsername(target.value)}
-         />
-       </div>
-       <div>
-         password
-         <input
-           type="password"
-           value={password}
-           onChange={({ target }) => setPassword(target.value)}
-         />
-     </div>
-       <button type="submit">login</button>
-     </form>
-   </div>
- )
+  LoginForm.propTypes = {
+    errorMessageRed: PropTypes.func.isRequired,
+    setErrorMessageRed: PropTypes.func.isRequired,
+    setUser: PropTypes.func.isRequired
+  }
+  return (
+    <div>
+      <NotificationRed message={errorMessageRed} />
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginForm
