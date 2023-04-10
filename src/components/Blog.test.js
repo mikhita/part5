4f4a22254239713979a2d5be7c79f6a62302f9b2
mikhita <mikheilispiriani@yahoo.com>
2023-04-10 20:28:31 +0400

    Make a test, which checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked. is done

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title', () => {
@@ -16,3 +17,24 @@ test('renders title', () => {
     'mikhita'
   )
 })
+
+test('clicking the view button shows blog URL and likes', async () => {
+  const blog = {
+    title: 'mikhita',
+    author: 'Mikhita',
+    url: 'mikhita.com',
+    likes: 87
+  }
+
+  render(
+    <Blog blog={blog} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText(blog.url)).toBeInTheDocument()
+  expect(screen.getByText(`${blog.likes}`)).toBeInTheDocument()
+})
+
