9148a4bab6a1ad202392765cb85d51375ee14cf6
mikhita <mikheilispiriani@yahoo.com>
2023-06-05 23:54:22 +0400

    adding functionality to test adding the new blog

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -23,20 +23,37 @@ describe('Blog app', () => {
       cy.get('#login-button').click()
 
       cy.contains('Mikhita logged in')
-    })
-
-    it('fails with wrong credentials', function() {
-      cy.contains('log in').click()
-      cy.get('#username').type('mikhitada')
-      cy.get('#password').type('wrongpassword')
-      cy.get('#login-button').click()
 
       cy.wait(1000) // Wait for 1 second
-      cy.screenshot()
-      cy.contains('wrong username or password')
-        .should('be.visible')
-        .invoke('css', 'color')
-        .should('equal', 'rgb(255, 0, 0)')
+      cy.contains('new blog').click()
+      cy.get('#title').type('a blogTitle created by cypress')
+      cy.get('#author').type('author created by cypress')
+      cy.get('#url').type('url created by cypress')
+      cy.contains('create').click()
+      cy.contains('a blogTitle created by cypress')
     })
+
+    // it('fails with wrong credentials', function() {
+    //   cy.contains('log in').click()
+    //   cy.get('#username').type('mikhitada')
+    //   cy.get('#password').type('wrongpassword')
+    //   cy.get('#login-button').click()
+
+    //   cy.wait(1000) // Wait for 1 second
+    //   cy.screenshot()
+    //   cy.contains('wrong username or password')
+    //     .should('be.visible')
+    //     .invoke('css', 'color')
+    //     .should('equal', 'rgb(255, 0, 0)')
+    // })
+    // it('a new blog can be created', function() {
+    //   cy.wait(2000) // Wait for 1 second
+    //   cy.contains('new blog').click()
+    //   cy.get('#title').type('a blogTitle created by cypress')
+    //   cy.get('#author').type('author created by cypress')
+    //   cy.get('#url').type('url created by cypress')
+    //   cy.contains('create').click()
+    //   cy.contains('a blogTitle created by cypress')
+    // })
   })
 })
